793c208afc24f3b6a4b38d112bae350784a3a95d
Yutaka Shimada <shimayu22@gmail.com>
2019-02-17 22:26:01 +0900

    画像を送信する

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(originalContentUrl="https://www.shimay.uno/nekoguruma/wp-content/uploads/sites/2/2018/03/20171106_212850-973x649.jpg",previewImageUrl="https://www.shimay.uno/nekoguruma/wp-content/uploads/sites/2/2018/03/20171106_212850-508x339.jpg"))
 
 
 if __name__ == "__main__":
